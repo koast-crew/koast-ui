deaf6295118d768152a0d5b63dce246869b9332b
주다훤 <judahwon@koast.net>
2025-08-12 20:05:33 +0900

    fix: Select 컴포넌트 value값 제네릭 타입 정보 유지한다

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import './styles/tailwind.css';
 
-export { default as Button, type ButtonProps } from './components/Button';
+export { default as Button } from './components/Button';
 // export { default as CesiumMap, type CesiumMapProps } from './components/CesiumMap';
 export { default as ButtonGroup } from './components/ButtonGroup';
 export { default as FolderTree, type TreeNode } from './components/FolderTree';
-export { default as MapLegend, type MapLegendProps } from './components/MapLegend';
-export { default as Select, SelectItem, type SelectProps } from './components/Select';
+export { default as MapLegend } from './components/MapLegend';
+export { default as Select, SelectItem } from './components/Select';
 export { default as TimeSlider } from './components/TimeSlider';
-export type { DateToStringFunc, StepTimeSliderOnChangeProps, StepTimeSliderProps, TimeSliderSize, TimeSliderTheme, TimeUnit } from './components/TimeSlider';
 
+export type { ButtonProps } from './components/Button/Button.types';
+export type { MapLegendProps } from './components/MapLegend/MapLegend.types';
+export type { SelectProps, SelectItemProps, SelectSize, SelectVariant } from './components/Select/Select.types';
+export type { DateToStringFunc, StepTimeSliderOnChangeProps, StepTimeSliderProps, TimeSliderSize, TimeSliderTheme, TimeUnit } from './components/TimeSlider';
